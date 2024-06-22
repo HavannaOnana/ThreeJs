4a490d3c3b5189ef9e1f18338664c5f7578fcffc
HavannaOnana <138626351+HavannaOnana@users.noreply.github.com>
2024-06-22 23:51:52 +0200

    made the earth

--- ThreeJsSketches/index.js
+++ ThreeJsSketches/index.js
@@ -1,10 +1,10 @@
 import * as THREE from "three";
+import { OrbitControls } from "jsm/controls/OrbitControls.js";
 
 // making a renderer
 const renderer= new THREE.WebGLRenderer({antialias : true});
 //setting the width and height
 renderer.setSize(window.innerWidth,window.innerHeight);
-
 document.body.appendChild(renderer.domElement);
 
 let fov = 75;
@@ -22,23 +22,41 @@ const scene = new THREE.Scene()
 
 
 // adding some goe 
-const geo = new THREE.IcosahedronGeometry(1.0,2);
+const geo = new THREE.IcosahedronGeometry(1,12);
+
+//creating a new loader
+const loader = new THREE.TextureLoader();
 
 //adding a new material
-const material = new THREE.MeshBasicMaterial({
-    color:"biege"
+const material = new THREE.MeshStandardMaterial({
+    map : loader.load("./textures/earth.jpg")
 });
 
-//we are meshing it 
-const mesh = new THREE.Mesh(geo,material);
 
-scene.add(mesh);
+//we are meshing it 
+const earthMesh = new THREE.Mesh(geo,material);
+
+scene.add(earthMesh);
+//making a new light hemilight
+const light = new THREE.HemisphereLight();
+scene.add(light);
+
+
+
+
+// adding the orbitcontrols camera
+const controls = new OrbitControls(camera,renderer.domElement);
+controls.enableDamping = true;
+controls.dampingFactor = 0.02;
+
+
 
 // we adding the function animate
-function animate(t = 0){
+function animate( t= 0){
     requestAnimationFrame(animate);
-    mesh.scale.setScalar(Math.cos(t * 0.001) + 1.0);
+    earthMesh.rotation.y = t * 0.0001;
     renderer.render(scene,camera);
+    controls.update();
 }
 
 animate();
